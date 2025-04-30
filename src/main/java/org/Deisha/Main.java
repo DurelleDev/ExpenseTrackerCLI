0e6e3d926bbc07229db454b2da9353202f874033
Durelle Abdul <Durelle.abdul.07@gmail.com>
2025-04-30 11:30:47 +0200

    Adding Query methods
    Changed from Jackson to normal JSON to fully understand it

--- src/main/java/org/Deisha/Main.java
+++ src/main/java/org/Deisha/Main.java
@@ -14,20 +14,20 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
 
+        dbFunctions dbFunctions = new dbFunctions();
+        dbFunctions.connectToDb("expensedb", "postgres", "22952");
+
         int expenseID;
         LocalDate expenseDate;
-        String expenseDesc;
+        String expenseDescription;
         double expenseAmt = 0;
         int count = 0;
 
-        System.out.println();
-
         Scanner userInput = new Scanner(System.in);
-        //HashMap<String, Object> expenseData = new HashMap<>();
 
         //Getting user data for expenses
         System.out.println("Please enter a description: ");
-        expenseDesc = userInput.next();
+        expenseDescription = userInput.next();
 
         try{
             System.out.println("Please enter the amount: ");
@@ -41,38 +41,6 @@ public class Main {
         String expDate = expenseDate.toString();
         expenseID = count + 1;
 
-        //hash to store data in JSON
-//        expenseData.put("expenseID", expenseID);
-//        expenseData.put("expenseDate", expenseDate);
-//        expenseData.put("expenseDescription", expenseDesc);
-//        expenseData.put("expenseAmount", expenseAmt);
-
-
-        //ObjectMapper (Used to convert data between Java objects and JSON)
-        ObjectMapper objectMapper = new ObjectMapper(); // create
-        objectMapper.enable(SerializationFeature.INDENT_OUTPUT); //Enable pretty printing
-
-       try
-       {
-           //Expense(int expenseID, Instant expenseDate, String expenseDesc, double expenseAmt
-            String jsonString = objectMapper.writeValueAsString(new Expense(expenseID, expDate, expenseDesc, expenseAmt));
-            ExpenseJsonStorage.storeJson(jsonString);
-
-       }
-       catch (JsonProcessingException e){
-           //e.printStackTrace();
-           System.out.println(e);
-        }
-
-
-        // File file = new File("expense_data_jackson.json");
-
-//        try{
-//            objectMapper.writeValue(file, userInput);
-//        }
-//        catch (IOException e){
-//            e.printStackTrace();
-//        }
     }
 
 }
