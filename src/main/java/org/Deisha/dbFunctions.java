392b8d4382f164a55cb5280d18d1315f5d01463e
Durelle Abdul <Durelle.abdul.07@gmail.com>
2025-05-02 11:47:39 +0200

    Connect to PostgreSQL database

--- src/main/java/org/Deisha/dbFunctions.java
+++ src/main/java/org/Deisha/dbFunctions.java
@@ -1,5 +1,30 @@
 package org.Deisha;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class dbFunctions {
+
+    public Connection connectToDb(String JDBC_DBNAME, String JDBC_USER, String JDBC_PASSWORD){
+        Connection connection = null;
+
+        try{
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + JDBC_DBNAME, JDBC_USER, JDBC_PASSWORD);
+            if (connection != null)
+                System.out.println("Connection Successful!!!");
+            else
+                System.out.println("FAILED TO CONNECT TO DATABASE!!");
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        return connection;
+    }
+
+
+
+
+
+
 
 }
