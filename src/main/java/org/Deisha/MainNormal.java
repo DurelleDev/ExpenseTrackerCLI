989867e6ef4aeb0cbe0f8b452e44a4fb6b1b9c3a
Durelle Abdul <Durelle.abdul.07@gmail.com>
2025-05-05 11:15:01 +0200

    View Summary & View all
    User can view all data in the expense table and display a summary

--- src/main/java/org/Deisha/MainNormal.java
+++ src/main/java/org/Deisha/MainNormal.java
@@ -1,27 +1,56 @@
 package org.Deisha;
 
-import java.sql.Connection;
 import java.time.LocalDate;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class MainNormal {
     public static void main(String [] args){
-        String expenseDesc, expenseDate;
+        String expenseDesc, expenseDate, deleteExp;
+        char userChoice;
         double expenseAmt;
 
         dbFunctions db = new dbFunctions("expensedb", "postgres", "22952");
         db.connectToDb();
 
         Scanner userInput = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("======================\nWhat do you want to do?\n=======================\nA. Display the Expenses?\nB. Add an expense?\nC. Delete an expense?\nD. Summary of the expenses");
+        userChoice = Character.toUpperCase(userInput.nextLine().charAt(0));
+        switch(userChoice) {
+            case 'A':
+                db.displayExpenses();
+                break;
 
-        System.out.println("Whats the description of the expense?\n");
-        expenseDesc = userInput.nextLine();
-        System.out.println("Whats the Expense amount of "+expenseDesc+": \n");
-        expenseAmt = userInput.nextDouble();
-        expenseDate = String.valueOf(LocalDate.now());
+            case 'B':
+                System.out.println("Whats the description of the expense?\n");
+                expenseDesc = userInput.nextLine();
+                System.out.println("Whats the Expense amount of "+expenseDesc+": \n");
+                expenseAmt = userInput.nextDouble();
+                expenseDate = String.valueOf(LocalDate.now());
+                db.addExpense(expenseDesc,expenseAmt,expenseDate);
+                break;
 
-        db.addExpense(expenseDesc,expenseAmt,expenseDate);
+            case 'C':
+                System.out.println("What expense do you want to delete? ");
+                deleteExp = userInput.nextLine();
+                db.deleteExpense(deleteExp);
+                break;
+            case 'D':
+                    db.displaySummary();
+                break;
+
+            default:
+                System.out.println("Invalid choice");
+        }
+
+
+
+//        System.out.println("Whats the description of the expense?\n");
+//        expenseDesc = userInput.nextLine();
+//        System.out.println("Whats the Expense amount of "+expenseDesc+": \n");
+//        expenseAmt = userInput.nextDouble();
+//        expenseDate = String.valueOf(LocalDate.now());
+//        db.addExpense(expenseDesc,expenseAmt,expenseDate);
 
     }
 }
--- src/main/java/org/Deisha/dbFunctions.java
+++ src/main/java/org/Deisha/dbFunctions.java
@@ -1,8 +1,6 @@
 package org.Deisha;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.*;
 
 public class dbFunctions {
     private Connection conn = null;
@@ -32,7 +30,49 @@ public class dbFunctions {
         this.conn = connection;
     }
 
+    public void displayExpenses(){
+        Statement statement;
+        ResultSet displayResult;
+        try{
+            statement = conn.createStatement();
+            String query = "SELECT * FROM expenses";
+            displayResult = statement.executeQuery(query);
+            System.out.println("Sucess!!\n");
 
+            while(displayResult.next()){
+                System.out.print(displayResult.getString("expense_id") + " ");
+                System.out.print(displayResult.getString("expense_description") + " ");
+                System.out.print(displayResult.getString("expense_amount") + " ");
+                System.out.println(displayResult.getString("expense_date"));
+            }
+        }
+        catch (SQLException e){
+            System.out.println("FAILED! (displayExpense)");
+            System.out.println(e);
+        }
+    }
+
+    public void displaySummary(){
+        Statement statement;
+        ResultSet results;
+        double expenses = 0;
+
+        try{
+            statement = conn.createStatement();
+            String query = String.format("SELECT * FROM expenses");
+            results = statement.executeQuery(query);
+
+            while(results.next()){
+                expenses += results.getDouble("expense_amount");
+            }
+
+            System.out.println("Total expenses: R"+expenses);
+
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+
+    }
 
     public void addExpense(String expenseDesc, double expenseAmt, String expenseDate){
         Statement statement;
@@ -41,10 +81,30 @@ public class dbFunctions {
             statement = conn.createStatement();
             String query = String.format("INSERT INTO expenses(expense_description, expense_amount, expense_date) VALUES ('%s',"+ expenseAmt +", '%s')", expenseDesc, expenseDate);
             statement.executeUpdate(query);
+            System.out.println("added the expense: "+expenseDesc);
         }
         catch (Exception e){
             System.out.println(e);
         }
+    }
+
+    public void deleteExpense(String expDesc){
+        Statement statement;
+
+        try{
+            statement = conn.createStatement();
+            String query = String.format("DELETE FROM expenses WHERE expense_description = '%s'", expDesc);
+            statement.executeUpdate(query);
+            System.out.println("Successfully deleted: "+ expDesc);
+        }
+        catch (SQLException e)
+        {
+            System.out.println("FAILED (deleteExpense");
+            System.out.println(e);
+        }
+
+
+
     }
 
 
